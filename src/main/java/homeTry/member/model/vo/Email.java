9acefb99bed73dc6c8aa16d4c4a501d7ce0acbf1
rdme0 <we6610@gmail.com>
2024-09-20 17:49:17 +0900

    fix: vo가 값을 검증하지 않는 버그 해결

--- src/main/java/homeTry/member/model/vo/Email.java
+++ src/main/java/homeTry/member/model/vo/Email.java
@@ -4,12 +4,16 @@ import java.util.regex.Pattern;
 
 public record Email( String email ) {
 
-    private static final Pattern EMAIL_PATTERN = Pattern.compile(
-            "^[A-Za-z0-9+_.-]+@(.+)$"
-    );
+    public Email {
+        validateEmail(email);
+    }
 
     private void validateEmail(String email) {
         if (email == null || !EMAIL_PATTERN.matcher(email).matches())
             throw new IllegalArgumentException("올바른 이메일 형식이 아닙니다.");
     }
+
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(
+            "^[A-Za-z0-9+_.-]+@(.+)$"
+    );
 }
--- src/main/java/homeTry/member/model/vo/Password.java
+++ src/main/java/homeTry/member/model/vo/Password.java
@@ -2,6 +2,10 @@ package homeTry.member.model.vo;
 
 public record Password(String password) {
 
+    public Password {
+        validatePassword(password);
+    }
+
     private void validatePassword(String password) {
         if (password == null || password.isBlank())
             throw new IllegalArgumentException("비밀번호를 입력해주세요.");
