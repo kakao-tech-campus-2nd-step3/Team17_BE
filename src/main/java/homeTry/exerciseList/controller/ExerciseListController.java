6ec677f01ac9343a93d5a97b901dc73d606992e4
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-09-23 23:30:06 +0900

    refactor: 헤더에서 @RequestHeader 삭제

--- src/main/java/homeTry/exerciseList/controller/ExerciseListController.java
+++ src/main/java/homeTry/exerciseList/controller/ExerciseListController.java
@@ -2,7 +2,6 @@ package homeTry.exerciseList.controller;
 
 import homeTry.exerciseList.service.ExerciseListService;
 import homeTry.exerciseList.dto.ExerciseListRequest;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,32 +23,28 @@ public class ExerciseListController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> createExercise(@RequestBody ExerciseListRequest request,
-        @RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader) {
+    public ResponseEntity<Void> createExercise(@RequestBody ExerciseListRequest request) {
 
         exerciseListService.createExercise(request);  // 운동 생성
         return new ResponseEntity<>(HttpStatus.CREATED);  // 상태 코드 201
     }
 
     @DeleteMapping("/{exerciseId}")
-    public ResponseEntity<Void> deleteExercise(@PathVariable Long exerciseId,
-        @RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader) {
+    public ResponseEntity<Void> deleteExercise(@PathVariable Long exerciseId) {
 
         exerciseListService.deleteExercise(exerciseId);  // 운동 삭제
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);  // 상태 코드 204
     }
 
     @PutMapping("/{exerciseId}/start")
-    public ResponseEntity<Void> startExercise(@PathVariable Long exerciseId,
-        @RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader) {
+    public ResponseEntity<Void> startExercise(@PathVariable Long exerciseId) {
 
         exerciseListService.startExercise(exerciseId);
         return new ResponseEntity<>(HttpStatus.OK);  // 상태 코드 200
     }
 
     @PutMapping("/{exerciseId}/stop")
-    public ResponseEntity<Void> stopExercise(@PathVariable Long exerciseId,
-        @RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader) {
+    public ResponseEntity<Void> stopExercise(@PathVariable Long exerciseId) {
 
         exerciseListService.stopExercise(exerciseId);
         return new ResponseEntity<>(HttpStatus.OK);  // 상태 코드 200
