205c985fd8d3af909a4e636fc632ea336d0b766b
rdme0 <71927381+rdme0@users.noreply.github.com>
2024-09-24 10:50:15 +0900

    feat: Argument Resolver 구현

---
+++ src/main/java/homeTry/auth/LoginMemberArgumentResolver.java
@@ -0,0 +1,44 @@
+package homeTry.auth;
+
+import homeTry.annotation.LoginMember;
+import homeTry.exception.clientException.BadRequestException;
+import homeTry.exception.clientException.UserNotFoundException;
+import homeTry.exception.serverException.InternalServerException;
+import homeTry.member.service.MemberService;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final MemberService memberService;
+    private final JwtAuth jwtAuth;
+
+    public LoginMemberArgumentResolver(MemberService memberService, JwtAuth jwtAuth) {
+        this.memberService = memberService;
+        this.jwtAuth = jwtAuth;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(LoginMember.class) != null;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        try{
+            String token = webRequest.getHeader("Authorization").substring(7);
+            String email = jwtAuth.extractEmail(token);
+            return memberService.getMember(email);
+        } catch (UserNotFoundException e) {
+            throw new UserNotFoundException("회원이 아닙니다.");
+        } catch (NullPointerException e){
+            throw new BadRequestException("토큰이 존재하지 않습니다.");
+        } catch (Exception e) {
+            throw new InternalServerException(e.getMessage());
+        }
+    }
+}
