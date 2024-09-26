ca1c10b7f1bdb17ef5f6f2de8a05ceb120d7832d
rdme0 <71927381+rdme0@users.noreply.github.com>
2024-09-26 11:09:38 +0900

    remove: 필요없는 의존성 제거
    Closes #56

--- src/main/java/homeTry/diary/controller/DiaryController.java
+++ src/main/java/homeTry/diary/controller/DiaryController.java
@@ -5,13 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import homeTry.diary.dto.request.DiaryRequest;
 import homeTry.diary.service.DiaryService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/diary")
