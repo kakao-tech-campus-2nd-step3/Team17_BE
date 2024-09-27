ac42e9f153b3bb033f68437c204b938aaddbeaf3
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2024-09-27 13:12:55 +0900

    feat: exception 상위 디렉토리 구성

---
+++ src/main/java/homeTry/exception/InternalServerException.java
@@ -0,0 +1,17 @@
+package homeTry.exception;
+
+public class InternalServerException extends RuntimeException{
+    
+    private final ErrorType errorType;
+
+    public InternalServerException(ErrorType errorType) {
+        super(errorType.getMessage());
+        this.errorType = errorType;
+    }
+
+    public ErrorType getErrorType (){
+        return errorType;
+    }
+
+
+}
