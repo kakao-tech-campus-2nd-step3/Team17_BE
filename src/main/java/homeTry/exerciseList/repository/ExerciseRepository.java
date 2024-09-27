e50eee64b63d9b48425ae09c84a2df107107445f
hxunsc <hyunsss21@naver.com>
2024-09-27 15:45:24 +0900

    refactor: ExerciseRepository 수정
    Optional 타입으로 변경하고, 메소드 이름 변경함

--- src/main/java/homeTry/exerciseList/repository/ExerciseRepository.java
+++ src/main/java/homeTry/exerciseList/repository/ExerciseRepository.java
@@ -1,14 +1,14 @@
 package homeTry.exerciseList.repository;
 
 import homeTry.exerciseList.model.entity.Exercise;
-import java.time.LocalDateTime;
-import java.util.List;
+import homeTry.member.model.vo.Email;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 
-    List<Exercise> findByStartTimeBetween(LocalDateTime startDate, LocalDateTime endDate);
+    Optional<Exercise> findByIdAndMemberEmail(Long exerciseId, Email memberEmail);
 
 }
