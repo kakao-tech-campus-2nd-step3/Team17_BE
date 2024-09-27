0cdf35000992e481b39f1dcc5c2c951dbd9ed461
Woojae Jeong <woo6388@naver.com>
2024-09-27 23:00:08 +0900

    refactor: TeamService의 addTeam 로직 수정

--- src/main/java/homeTry/team/service/TeamService.java
+++ src/main/java/homeTry/team/service/TeamService.java
@@ -4,9 +4,16 @@ import homeTry.member.dto.MemberDTO;
 import homeTry.member.model.entity.Member;
 import homeTry.member.service.MemberService;
 import homeTry.team.dto.RequestTeamDTO;
+import homeTry.team.dto.ResponseRanking;
 import homeTry.team.model.entity.Team;
+import homeTry.team.repository.TeamRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Service
 
@@ -22,19 +29,31 @@ public class TeamService {
 
     @Transactional
     public void addTeam(MemberDTO memberDTO, RequestTeamDTO requestTeamDTO) {
-        Member member = memberService.getMember(memberDTO.email());
         Team team = teamRepository.save(new Team(
                 requestTeamDTO.teamName(),
                 requestTeamDTO.teamDescription(),
-                member.getId(),
+                memberDTO.id(),
                 requestTeamDTO.maxParticipants(),
                 DEFAULT_PARTICIPANTS,
                 requestTeamDTO.password()
         ));
     }
 
-    @Transactional
-    public void deleteTeam(Long teamId) {
-        Member
+    /*
+    //Team과 member간의 ERD 다이어그램 수정 후 구현
+    @Transactional(readOnly = true)
+    public Page<ResponseRanking> getRanking(MemberDTO memberDTO, Pageable pageable, int year, int month, int day) {
+        LocalDate currentDate = LocalDate.now();
+        LocalDate targetDate = LocalDate.of(year, month, day);
+        if(targetDate.isBefore(currentDate))  // 현재 이전의 날짜에 대한 랭킹 요청 시
+            return getTodayRanking(pageable, currentDate, memberDTO);
+
+
+
     }
+    private Page<ResponseRanking> getTodayRanking(Pageable pageable, LocalDate currentDate, MemberDTO memberDTO) {
+        memberDTO
+    }
+    */
+
 }
