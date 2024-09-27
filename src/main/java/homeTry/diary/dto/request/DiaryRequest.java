b1ba4b16b67ebede9805a8ed7290aa942b0bf88b
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2024-09-27 13:07:39 +0900

    chore: DiaryRequest 코드 정리

--- src/main/java/homeTry/diary/dto/request/DiaryRequest.java
+++ src/main/java/homeTry/diary/dto/request/DiaryRequest.java
@@ -1,11 +1,5 @@
 package homeTry.diary.dto.request;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-
 public record DiaryRequest(String memo) {
 
-    @JsonValue
-    public String memo() {
-        return memo;
-    }
 }
