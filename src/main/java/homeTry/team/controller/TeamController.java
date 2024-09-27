baecb0ae467bed4c386d311957d58cbc8da4d9fe
Woojae Jeong <woo6388@naver.com>
2024-09-27 22:50:54 +0900

    refactor: TeamController 추후 개발부분 주석처리

--- src/main/java/homeTry/team/controller/TeamController.java
+++ src/main/java/homeTry/team/controller/TeamController.java
@@ -3,8 +3,13 @@ package homeTry.team.controller;
 import homeTry.annotation.LoginMember;
 import homeTry.member.dto.MemberDTO;
 import homeTry.team.dto.RequestTeamDTO;
+import homeTry.team.dto.ResponseRanking;
 import homeTry.team.service.TeamService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,16 +23,41 @@ public class TeamController {
         this.teamService = teamService;
     }
 
-    @PostMapping()
+    @PostMapping
     public ResponseEntity<Void> addTeam (@LoginMember MemberDTO memberDTO, @Valid RequestTeamDTO requestTeamDTO){
         teamService.addTeam(memberDTO, requestTeamDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    /*
+    //Team과 member간의 ERD 다이어그램 수정 후 구현
+    @GetMapping("/{teamId}/ranking")
+    public ResponseEntity<Page<ResponseRanking>> getRanking(@LoginMember MemberDTO memberDTO,
+                                                            @PageableDefault(size = 10, sort = "exerciseTime", direction = Sort.Direction.DESC) Pageable pageable,
+                                                            @PathVariable("teamId") Long teamId,
+                                                            @RequestParam("year") int year,
+                                                            @RequestParam("month") int month,
+                                                            @RequestParam("day") int day) {
+        Page<ResponseRanking> rankingPage = teamService.getRanking(memberDTO, pageable, year, month, day);
+        return ResponseEntity.ok(rankingPage);
+    }
+    */
+
+
+    /*
+    //태그 기능 제작 후 구현
+    @GetMapping("/form")
+    public ResponseEntity<ResponseTeamForm> (@LoginMember MemberDTO memberDTO){
+    }
+     */
+
+    /*
+    //Team과 member간의 ERD 다이어그램 수정 후 구현
     @DeleteMapping("/{teamId)")
     public ResponseEntity<Void> deleteTeam(@LoginMember MemberDTO memberDTO,
                                            @PathVariable("teamId") Long teamId){
         teamService.deleteTeam(teamId);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
+    */
 }
