069ead939de19f611ad0215eca25df90b2a4aa70
hxunsc <hyunsss21@naver.com>
2024-09-30 00:36:24 +0900

    refactor: ExerciseService 분리
    ExerciseService를 ExerciseTime, ExerciseHistory에 맞춰 기능 분리함

--- src/main/java/homeTry/exerciseList/repository/ExerciseRepository.java
+++ src/main/java/homeTry/exerciseList/repository/ExerciseRepository.java
@@ -1,6 +1,7 @@
 package homeTry.exerciseList.repository;
 
 import homeTry.exerciseList.model.entity.Exercise;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,5 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 
     Optional<Exercise> findByIdAndMemberId(Long exerciseId, Long memberId);
+
+    List<Exercise> findAllByMemberId(Long memberId);
 
 }
