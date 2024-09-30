5c327fa526a968941a6d66f667184d0aaff0fd1c
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2024-09-30 20:36:19 +0900

    refactor: MainPage의 Diary조회 리팩토링

--- src/main/java/homeTry/diary/dto/DiaryDto.java
+++ src/main/java/homeTry/diary/dto/DiaryDto.java
@@ -2,10 +2,15 @@ package homeTry.diary.dto;
 
 import java.time.LocalDateTime;
 
+import homeTry.diary.model.entity.Diary;
+
 public record DiaryDto(
     Long id,
     LocalDateTime createAt,
     String memo,
     String email
 ) {
+    public static DiaryDto convertToDiaryDto(Diary diary) {
+        return new DiaryDto(diary.getId(), diary.getCreateAt(), diary.getMemo().value(), diary.getMember().getEmail());
+    }
 }
