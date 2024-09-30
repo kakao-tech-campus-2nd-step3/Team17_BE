412289308b391165d90a634b79153d6736272717
hxunsc <hyunsss21@naver.com>
2024-09-30 18:41:43 +0900

    feat: 특정 하루에 대한 운동 기록 반환 구현

--- src/main/java/homeTry/exerciseList/service/ExerciseHistoryService.java
+++ src/main/java/homeTry/exerciseList/service/ExerciseHistoryService.java
@@ -28,6 +28,18 @@ public class ExerciseHistoryService {
         }
     }
 
+    // 특정 날에 대한 운동 시간 반환
+    @Transactional(readOnly = true)
+    public Duration getExerciseHistoriesForDay(Long memberId, LocalDate date) {
+        LocalDateTime startOfDay = date.atTime(3, 0, 0);
+        LocalDateTime endOfDay = date.plusDays(1).atTime(2, 59, 59);
+
+        List<ExerciseHistory> exercises = exerciseHistoryRepository.findByExerciseMemberIdAndCreatedAtBetween(
+            memberId, startOfDay, endOfDay);
+
+        return sumExerciseTime(exercises);
+    }
+
     @Transactional(readOnly = true)
     public Duration getWeeklyTotalExercise(Long memberId) {
         // 이번 주의 시작과 끝 계산 (새벽 3시 기준), 하루 시작: 새벽 3시, 하루 끝: 다음날 새벽 2시 59분 59초
@@ -60,5 +72,4 @@ public class ExerciseHistoryService {
             .map(ExerciseHistory::getExerciseHistoryTime)
             .reduce(Duration.ZERO, Duration::plus);
     }
-
 }
--- src/main/java/homeTry/exerciseList/service/ExerciseTimeService.java
+++ src/main/java/homeTry/exerciseList/service/ExerciseTimeService.java
@@ -2,6 +2,10 @@ package homeTry.exerciseList.service;
 
 import homeTry.exerciseList.model.entity.ExerciseTime;
 import homeTry.exerciseList.repository.ExerciseTimeRepository;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +27,20 @@ public class ExerciseTimeService {
     public void resetExerciseTime(ExerciseTime exerciseTime) {
         exerciseTime.resetExerciseTime();
         saveExerciseTime(exerciseTime);
+    }
+
+    // 당일의 운동 시간 반환
+    @Transactional(readOnly = true)
+    public Duration getExerciseTimesForToday(Long memberId) {
+        LocalDateTime startOfDay = LocalDate.now().atTime(3, 0, 0);
+        LocalDateTime endOfDay = LocalDate.now().plusDays(1).atTime(2, 59, 59);
+
+        // 해당 멤버의 당일 운동 시간 목록 조회
+        List<ExerciseTime> exerciseTimes = exerciseTimeRepository.findByMemberIdAndStartTimeBetween(memberId, startOfDay, endOfDay);
+
+        return exerciseTimes.stream()
+            .map(ExerciseTime::getExerciseTime)
+            .reduce(Duration.ZERO, Duration::plus);
     }
 
 }
