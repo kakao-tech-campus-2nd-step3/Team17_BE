8188f02388270a20e244f79e1a04123f3a12821f
hxunsc <hyunsss21@naver.com>
2024-10-02 14:57:18 +0900

    feat: 운동 시작 관련 예외 처리 구현
    이미 한 운동이 시작하고 있을 때, 다른 운동 시작하려고 할 시 반환할 예외처리 구현

--- src/main/java/homeTry/exerciseList/exception/ExerciseErrorType.java
+++ src/main/java/homeTry/exerciseList/exception/ExerciseErrorType.java
@@ -5,14 +5,16 @@ import org.springframework.http.HttpStatus;
 
 public enum ExerciseErrorType implements ErrorType {
 
-    // 1. 운동 리스트를 찾을 수 없을 때 - 404
-    EXERCISE_NOT_FOUND_EXCEPTION("ExerciseList404_001", HttpStatus.NOT_FOUND, "해당 운동을 찾을 수 없습니다."),
-    // 2. 운동이 이미 진행 중일 때 - 400
-    EXERCISE_ALREADY_ACTIVE_EXCEPTION("ExerciseList400_001", HttpStatus.BAD_REQUEST, "이미 운동을 진행 중입니다."),
-    // 3. 운동이 시작되지 않았는데 종료를 시도할 때 - 400
-    EXERCISE_NOT_ACTIVE_EXCEPTION("ExerciseList400_002", HttpStatus.BAD_REQUEST, "운동이 시작되지 않았습니다."),
-    // 4. 운동에 대한 권한이 없을 때 - 403
-    EXERCISE_NO_PERMISSION_EXCEPTION("ExerciseList403_001", HttpStatus.FORBIDDEN, "이 운동에 대한 실행 권한이 없습니다.");
+    // 운동 리스트를 찾을 수 없을 때 - 404
+    EXERCISE_NOT_FOUND_EXCEPTION("Exercise404_001", HttpStatus.NOT_FOUND, "해당 운동을 찾을 수 없습니다."),
+    // 운동이 이미 진행 중일 때 - 400
+    EXERCISE_ALREADY_ACTIVE_EXCEPTION("Exercise400_001", HttpStatus.BAD_REQUEST, "이미 운동을 진행 중입니다."),
+    // 운동이 시작되지 않았는데 종료를 시도할 때 - 400
+    EXERCISE_NOT_ACTIVE_EXCEPTION("Exercise400_002", HttpStatus.BAD_REQUEST, "운동이 시작되지 않았습니다."),
+    // 이미 다른 운동이 시작 중일 때 - 400
+    ANOTHER_EXERCISE_IN_PROGRESS_EXCEPTION("Exercise400_003", HttpStatus.BAD_REQUEST, "다른 운동이 이미 진행 중입니다."),
+    // 운동에 대한 권한이 없을 때 - 403
+    EXERCISE_NO_PERMISSION_EXCEPTION("Exercise403_001", HttpStatus.FORBIDDEN, "이 운동에 대한 실행 권한이 없습니다.");
 
     private final String errorCode;
     private final HttpStatus httpStatus;
