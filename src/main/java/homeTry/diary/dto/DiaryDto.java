4250b66f565242fa4c6719b9046779e1ea1000f8
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2024-10-03 09:52:15 +0900

    refactor: DiaryDto 리팩토링

--- src/main/java/homeTry/diary/dto/DiaryDto.java
+++ src/main/java/homeTry/diary/dto/DiaryDto.java
@@ -6,11 +6,10 @@ import homeTry.diary.model.entity.Diary;
 
 public record DiaryDto(
     Long id,
-    LocalDateTime createAt,
-    String memo,
-    String email
+    LocalDateTime createdAt,
+    String memo
 ) {
     public static DiaryDto convertToDiaryDto(Diary diary) {
-        return new DiaryDto(diary.getId(), diary.getCreateAt(), diary.getMemo().value(), diary.getMember().getEmail());
+        return new DiaryDto(diary.getId(), diary.getCreateAt(), diary.getMemo().value());
     }
 }
