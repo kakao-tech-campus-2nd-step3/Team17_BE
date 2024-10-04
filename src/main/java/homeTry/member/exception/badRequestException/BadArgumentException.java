3b5cb318e1f0164faa39fdc55a80487f7d245cf0
rdme0 <we6610@gmail.com>
2024-10-04 21:31:32 +0900

    fix: 토큰 생성 에러 해결

--- src/main/java/homeTry/member/exception/badRequestException/BadArgumentException.java
+++ src/main/java/homeTry/member/exception/badRequestException/BadArgumentException.java
@@ -7,9 +7,6 @@ public class BadArgumentException extends BadRequestException {
 
     public BadArgumentException(String message) {
         super(MemberErrorType.BAD_ARGUMENT_EXCEPTION);
-
-        String defaultErrorMessage = MemberErrorType.BAD_ARGUMENT_EXCEPTION.getMessage();
-        MemberErrorType.BAD_ARGUMENT_EXCEPTION.setMessage(defaultErrorMessage + " : " + message);
     }
 
 }
