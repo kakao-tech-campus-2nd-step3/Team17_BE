9a05b069dceaa9a97f3269a7cfbc92ae71d73653
Woojae Jeong <woo6388@naver.com>
2024-10-04 23:09:34 +0900

    feat: 특정 TeamMember 엔티티를 찾도록, 특정 팀에 속한 모든 엔티티 삭제 기능 구현

--- src/main/java/homeTry/team/repository/TeamMemberRepository.java
+++ src/main/java/homeTry/team/repository/TeamMemberRepository.java
@@ -1,12 +1,15 @@
 package homeTry.team.repository;
 
+import homeTry.member.model.entity.Member;
 import homeTry.team.model.entity.Team;
 import homeTry.team.model.entity.TeamMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface TeamMemberRepository extends JpaRepository<TeamMember, Long> {
 
-    List<TeamMember> findByTeam(Team team);
+    Optional<TeamMember> findByTeamAndMember(Team team, Member member);
+
+    void deleteByTeam(Team team); //특정 팀에 속한 모든 엔티티 삭제
 }
