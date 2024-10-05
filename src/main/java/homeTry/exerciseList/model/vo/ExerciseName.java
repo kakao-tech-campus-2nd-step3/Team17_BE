8b463015957fd21945558682e6e13ae6f16b23e1
hxunsc <hyunsss21@naver.com>
2024-10-05 19:46:24 +0900

    docs: 운동기능관련 주석 추가

--- src/main/java/homeTry/exerciseList/model/vo/ExerciseName.java
+++ src/main/java/homeTry/exerciseList/model/vo/ExerciseName.java
@@ -12,10 +12,12 @@ public record ExerciseName(String value) {
     }
 
     private void validateExerciseName(String exerciseName) {
+        // 운동 이름 공백 불가
         if (exerciseName == null || exerciseName.isBlank()) {
             throw new ExerciseNameBlankException();
         }
 
+        // 운동 이름은 최대 20자 까지
         if (exerciseName.length() > 20) {
             throw new ExerciseNameTooLongException();
         }
