d5f6c8af25fadd73620adf2b4f91f7f705463f25
hxunsc <hyunsss21@naver.com>
2024-10-05 20:53:11 +0900

    refactor: 상수를 이용하여 시간 관련 수정

--- src/main/java/homeTry/exerciseList/service/ExerciseHistoryService.java
+++ src/main/java/homeTry/exerciseList/service/ExerciseHistoryService.java
@@ -1,5 +1,6 @@
 package homeTry.exerciseList.service;
 
+import homeTry.constants.DateTimeUtil;
 import homeTry.exerciseList.dto.ExerciseResponse;
 import homeTry.exerciseList.model.entity.Exercise;
 import homeTry.exerciseList.model.entity.ExerciseHistory;
@@ -33,8 +34,8 @@ public class ExerciseHistoryService {
     // 특정 날에 대한 운동 시간 반환
     @Transactional(readOnly = true)
     public Duration getExerciseHistoriesForDay(Long memberId, LocalDate date) {
-        LocalDateTime startOfDay = date.atTime(3, 0, 0);
-        LocalDateTime endOfDay = date.plusDays(1).atTime(2, 59, 59);
+        LocalDateTime startOfDay = DateTimeUtil.getStartOfDay(date);
+        LocalDateTime endOfDay = DateTimeUtil.getEndOfDay(date);
 
         List<ExerciseHistory> exercises = exerciseHistoryRepository.findByExerciseMemberIdAndCreatedAtBetween(
             memberId, startOfDay, endOfDay);
@@ -45,10 +46,8 @@ public class ExerciseHistoryService {
     @Transactional(readOnly = true)
     public Duration getWeeklyTotalExercise(Long memberId) {
         // 이번 주의 시작과 끝 계산 (새벽 3시 기준), 하루 시작: 새벽 3시, 하루 끝: 다음날 새벽 2시 59분 59초
-        LocalDate startOfWeek = LocalDate.now()
-            .minusDays(LocalDate.now().getDayOfWeek().getValue() - 1);
-        LocalDateTime startOfWeekWith3AM = startOfWeek.atTime(3, 0, 0);
-        LocalDateTime endOfWeekWith3AM = startOfWeek.plusDays(6).atTime(2, 59, 59);
+        LocalDateTime startOfWeekWith3AM = DateTimeUtil.getStartOfWeek(LocalDate.now());
+        LocalDateTime endOfWeekWith3AM = DateTimeUtil.getEndOfWeek(LocalDate.now());
 
         List<ExerciseHistory> weeklyExercises = exerciseHistoryRepository.findByExerciseMemberIdAndCreatedAtBetween(
             memberId, startOfWeekWith3AM, endOfWeekWith3AM);
@@ -59,9 +58,8 @@ public class ExerciseHistoryService {
     @Transactional(readOnly = true)
     public Duration getMonthlyTotalExercise(Long memberId) {
         // 이번 달의 시작과 끝 계산
-        LocalDate startOfMonth = LocalDate.now().withDayOfMonth(1);
-        LocalDateTime startOfMonthWith3AM = startOfMonth.atTime(3, 0, 0);
-        LocalDateTime endOfMonthWith3AM = startOfMonth.plusMonths(1).minusDays(1).atTime(2, 59, 59);
+        LocalDateTime startOfMonthWith3AM = DateTimeUtil.getStartOfMonth(LocalDate.now());
+        LocalDateTime endOfMonthWith3AM = DateTimeUtil.getEndOfMonth(LocalDate.now());
 
         List<ExerciseHistory> monthlyExercises = exerciseHistoryRepository.findByExerciseMemberIdAndCreatedAtBetween(
             memberId, startOfMonthWith3AM, endOfMonthWith3AM);
@@ -80,8 +78,8 @@ public class ExerciseHistoryService {
     // 특정 날에 대한 운동 기록 반환
     @Transactional(readOnly = true)
     public List<ExerciseResponse> getExerciseResponsesForDay(Long memberId, LocalDate date) {
-        LocalDateTime startOfDay = date.atTime(3, 0, 0);
-        LocalDateTime endOfDay = date.plusDays(1).atTime(2, 59, 59);
+        LocalDateTime startOfDay = DateTimeUtil.getStartOfDay(date);
+        LocalDateTime endOfDay = DateTimeUtil.getEndOfDay(date);
 
         List<ExerciseHistory> exerciseHistories = exerciseHistoryRepository.findByExerciseMemberIdAndCreatedAtBetween(
             memberId, startOfDay, endOfDay);
