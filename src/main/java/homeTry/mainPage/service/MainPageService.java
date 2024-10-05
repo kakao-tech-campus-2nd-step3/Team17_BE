4f1b110363dcf599a46cbc2818acb5a8e44ea7a4
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2024-10-05 14:54:45 +0900

    refactor: MainPageService 리팩토링

--- src/main/java/homeTry/mainPage/service/MainPageService.java
+++ src/main/java/homeTry/mainPage/service/MainPageService.java
@@ -16,16 +16,13 @@ public class MainPageService {
 
     private final DiaryService diaryService;
     private final ExerciseTimeService exerciseTimeService;
-    private final ExerciseService exerciseService;
     private final ExerciseHistoryService exerciseHistoryService;
 
     public MainPageService(DiaryService diaryService, 
                             ExerciseTimeService exerciseTimeService, 
-                            ExerciseService exerciseService,
                             ExerciseHistoryService exerciseHistoryService) {
         this.diaryService = diaryService;
         this.exerciseTimeService = exerciseTimeService;
-        this.exerciseService = exerciseService;
         this.exerciseHistoryService = exerciseHistoryService;
     }
     
@@ -37,14 +34,14 @@ public class MainPageService {
 
             return new MainPageResponse(
                 exerciseTimeService.getExerciseTimesForToday(memberId), 
-                exerciseService.getExercisesForMember(memberId), 
+                exerciseTimeService.getExerciseResponsesForToday(memberId),
                 diaryService.getDiaryByDate(date, memberId));
 
         } else {
 
             return new MainPageResponse(
                 exerciseHistoryService.getExerciseHistoriesForDay(memberId, date), 
-                null, 
+                exerciseHistoryService.getExerciseResponsesForDay(memberId, date),
                 diaryService.getDiaryByDate(date, memberId));
         }
     }
