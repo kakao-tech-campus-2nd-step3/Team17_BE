9f66fb1557d5351e7353d86801ce87d52bd8a5af
rdme0 <we6610@gmail.com>
2024-10-06 15:48:17 +0900

    refactor: JsonProperty -> JsonNaming 으로 리팩터링

--- src/main/java/homeTry/auth/kakaoAuth/dto/KakaoMemberInfoDTO.java
+++ src/main/java/homeTry/auth/kakaoAuth/dto/KakaoMemberInfoDTO.java
@@ -1,33 +1,29 @@
 package homeTry.auth.kakaoAuth.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.Email;
 import java.util.Date;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoMemberInfoDTO(
-        @JsonProperty("id")
         Long id,
 
-        @JsonProperty("connected_at")
         Date connectedAt,
 
-        @JsonProperty("kakao_account")
         KakaoAccount kakaoAccount
 ) {
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
     public record KakaoAccount(
-            @JsonProperty("profile_nickname_needs_agreement")
             Boolean profileNicknameNeedsAgreement,
 
-            @JsonProperty("has_email")
             Boolean hasEmail,
 
-            @JsonProperty("email")
             @Email(message = "올바른 이메일 형식이 아닙니다.")
             String email,
 
-            @JsonProperty("profile")
             Profile profile
     ) {
-        public record Profile(@JsonProperty("nickname") String nickname) { }
+        public record Profile(String nickname) { }
     }
 }
