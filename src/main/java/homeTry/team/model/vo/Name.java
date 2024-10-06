df8946b5f187dbd228d21aa2e60fcc601a93c1a7
Woojae Jeong <woo6388@naver.com>
2024-10-06 15:59:01 +0900

    feat: Name의 이름 길이 유효성 검사 추가

--- src/main/java/homeTry/team/model/vo/Name.java
+++ src/main/java/homeTry/team/model/vo/Name.java
@@ -13,5 +13,7 @@ public record Name (String value) {
     private void validateName(String value) {
         if(value == null || value.isBlank())
             throw new IllegalArgumentException("이름값은 필수입니다");
+        if(value.length() > 15)
+            throw new IllegalArgumentException("이름의 길이는 최대 15자 입니다");
     }
 }
