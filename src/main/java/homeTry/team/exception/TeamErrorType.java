4fd79d7b961c28f2e5148f959727f02d695efe49
Woojae Jeong <woo6388@naver.com>
2024-10-06 16:39:54 +0900

    feat: TeamErrorType 및 예외 구현

---
+++ src/main/java/homeTry/team/exception/TeamErrorType.java
@@ -0,0 +1,35 @@
+package homeTry.team.exception;
+
+import homeTry.exception.ErrorType;
+import org.springframework.http.HttpStatus;
+
+public enum TeamErrorType implements ErrorType {
+    TEAM_NOT_FOUND_EXCEPTION ("Team404_001", HttpStatus.NOT_FOUND, "해당 팀을 찾을 수 없습니다"),
+    TEAM_NAME_ALREADY_EXISTS_EXCEPTION("Team409_001", HttpStatus.CONFLICT, "이미 동일한 이름의 팀이 존재합니다"),
+    NOT_TEAM_LEADER_EXCEPTION("Team401_001", HttpStatus.UNAUTHORIZED, "리더만이 삭제할 수 있습니다.");
+
+    private String errorCode;
+    private HttpStatus httpStatus;
+    private String message;
+
+    TeamErrorType(String errorCode, HttpStatus httpStatus, String message) {
+        this.errorCode = errorCode;
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+
+    @Override
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
