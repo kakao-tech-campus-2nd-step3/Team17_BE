9be3c3d3891f9903ae1811170e38d48952e5520e
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2024-10-06 12:35:11 +0900

    refactor: DiaryRequest 리팩토링

--- src/main/java/homeTry/diary/dto/request/DiaryRequest.java
+++ src/main/java/homeTry/diary/dto/request/DiaryRequest.java
@@ -1,5 +1,21 @@
 package homeTry.diary.dto.request;
 
-public record DiaryRequest(String memo) {
+import homeTry.diary.exception.BadRequestException.MemoBlankException;
+import homeTry.diary.exception.BadRequestException.MemoTooLongException;
 
+public record DiaryRequest(String memo) {
+    private static final int MAX_LENGTH = 500;
+
+    public DiaryRequest {
+        validateMemo(memo);  
+    }
+
+    private static void validateMemo(String memo) {
+        if (memo != null && memo.isBlank()) {
+            throw new MemoBlankException();
+        }
+        if (memo != null && memo.length() > MAX_LENGTH) {
+            throw new MemoTooLongException();
+        }
+    }
 }
