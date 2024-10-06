e3edc8646b80fe56fa98ddc970e0d886ab21383d
Woojae Jeong <woo6388@naver.com>
2024-10-06 17:00:35 +0900

    feat: TeamMemberNotFoundException 구현

--- src/main/java/homeTry/team/exception/TeamErrorType.java
+++ src/main/java/homeTry/team/exception/TeamErrorType.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 public enum TeamErrorType implements ErrorType {
     TEAM_NOT_FOUND_EXCEPTION ("Team404_001", HttpStatus.NOT_FOUND, "해당 팀을 찾을 수 없습니다"),
     TEAM_NAME_ALREADY_EXISTS_EXCEPTION("Team409_001", HttpStatus.CONFLICT, "이미 동일한 이름의 팀이 존재합니다"),
-    NOT_TEAM_LEADER_EXCEPTION("Team401_001", HttpStatus.UNAUTHORIZED, "리더만이 삭제할 수 있습니다.");
+    NOT_TEAM_LEADER_EXCEPTION("Team401_001", HttpStatus.UNAUTHORIZED, "리더만이 삭제할 수 있습니다."),
+    TEAMMEMBER_NOT_FOUND_EXCEPTION("Team500_001", HttpStatus.NOT_FOUND, "해당 팀멤버를 찾을 수 없습니다");
 
     private String errorCode;
     private HttpStatus httpStatus;
