614c9081d1253aac12eb3a5006b7cd0ed4c1d359
hxunsc <hyunsss21@naver.com>
2024-10-06 11:18:31 +0900

    refactor: 중복된 예외 처리 삭제
    활성화된 운동에 대한 체크는 한 번으로 수정

--- src/main/java/homeTry/exerciseList/exception/ExerciseErrorType.java
+++ src/main/java/homeTry/exerciseList/exception/ExerciseErrorType.java
@@ -8,21 +8,19 @@ public enum ExerciseErrorType implements ErrorType {
     // 운동 리스트를 찾을 수 없을 때 - 404
     EXERCISE_NOT_FOUND_EXCEPTION("Exercise404_001", HttpStatus.NOT_FOUND, "해당 운동을 찾을 수 없습니다."),
     // 운동이 이미 진행 중일 때 - 400
-    EXERCISE_ALREADY_ACTIVE_EXCEPTION("Exercise400_001", HttpStatus.BAD_REQUEST, "이미 해당 운동을 진행 중입니다."),
+    EXERCISE_ALREADY_ACTIVE_EXCEPTION("Exercise400_001", HttpStatus.BAD_REQUEST, "이미 운동을 진행 중입니다."),
     // 운동이 시작되지 않았는데 종료를 시도할 때 - 400
     EXERCISE_NOT_ACTIVE_EXCEPTION("Exercise400_002", HttpStatus.BAD_REQUEST, "운동이 시작되지 않았습니다."),
-    // 이미 다른 운동이 시작 중일 때 - 400
-    ANOTHER_EXERCISE_IN_PROGRESS_EXCEPTION("Exercise400_003", HttpStatus.BAD_REQUEST, "다른 운동이 이미 진행 중입니다."),
     // 한 번에 운동 시간 제한 초과 - 400
-    EXERCISE_TIME_LIMIT_EXCEEDED_EXCEPTION("Exercise400_004", HttpStatus.BAD_REQUEST, "한 번에 저장되는 운동 시간은 8시간을 초과할 수 없습니다."),
+    EXERCISE_TIME_LIMIT_EXCEEDED_EXCEPTION("Exercise400_003", HttpStatus.BAD_REQUEST, "한 번에 저장되는 운동 시간은 8시간을 초과할 수 없습니다."),
     // 하루 운동 시간 제한 초과 - 400
-    DAILY_EXERCISE_TIME_LIMIT_EXCEEDED_EXCEPTION("Exercise400_005", HttpStatus.BAD_REQUEST, "하루 전체 운동 시간은 12시간을 초과할 수 없습니다."),
+    DAILY_EXERCISE_TIME_LIMIT_EXCEEDED_EXCEPTION("Exercise400_004", HttpStatus.BAD_REQUEST, "하루 전체 운동 시간은 12시간을 초과할 수 없습니다."),
     // 삭제한 운동을 시작하려고 할 때 - 400
-    EXERCISE_DEPRECATED_EXCEPTION("Exercise400_006", HttpStatus.BAD_REQUEST, "이 운동은 삭제된 운동으로, 운동을 시작할 수 없습니다."),
+    EXERCISE_DEPRECATED_EXCEPTION("Exercise400_005", HttpStatus.BAD_REQUEST, "이 운동은 삭제된 운동으로, 운동을 시작할 수 없습니다."),
     // 운동 이름이 공백일 때 - 400
-    EXERCISE_NAME_BLANK_EXCEPTION("Exercise400_007", HttpStatus.BAD_REQUEST, "운동 이름은 필수입니다."),
+    EXERCISE_NAME_BLANK_EXCEPTION("Exercise400_006", HttpStatus.BAD_REQUEST, "운동 이름은 필수입니다."),
     // 운동 이름이 너무 길 때 - 400
-    EXERCISE_NAME_TOO_LONG_EXCEPTION("Exercise400_008", HttpStatus.BAD_REQUEST, "운동 이름은 최대 20자까지 가능합니다."),
+    EXERCISE_NAME_TOO_LONG_EXCEPTION("Exercise400_007", HttpStatus.BAD_REQUEST, "운동 이름은 최대 20자까지 가능합니다."),
     // 운동에 대한 권한이 없을 때 - 403
     EXERCISE_NO_PERMISSION_EXCEPTION("Exercise403_001", HttpStatus.FORBIDDEN, "이 운동에 대한 실행 권한이 없습니다.");
 
