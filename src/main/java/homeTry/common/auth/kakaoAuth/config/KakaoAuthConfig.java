b107c5fe5677da254eecfb7a93e0b87d4a41f7b7
rdme0 <71927381+rdme0@users.noreply.github.com>
2024-10-08 11:09:22 +0900

    refactor(Auth): Config 파일 immutable 하도록 리팩터링

--- src/main/java/homeTry/common/auth/kakaoAuth/config/KakaoAuthConfig.java
+++ src/main/java/homeTry/common/auth/kakaoAuth/config/KakaoAuthConfig.java
@@ -1,49 +1,11 @@
 package homeTry.common.auth.kakaoAuth.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
-@Configuration
-@PropertySource({"classpath:application-kakao-login.properties",
-    "classpath:application-secret.properties"})
 @ConfigurationProperties(prefix = "kakao")
-public class KakaoAuthConfig {
-
-    private String restApiKey;
-    private String redirectUri;
-    private String tokenUrl;
-    private String userInfoUrl;
-
-    public String getRestApiKey() {
-        return restApiKey;
-    }
-
-    public void setRestApiKey(String restApiKey) {
-        this.restApiKey = restApiKey;
-    }
-
-    public String getRedirectUri() {
-        return redirectUri;
-    }
-
-    public void setRedirectUri(String redirectUri) {
-        this.redirectUri = redirectUri;
-    }
-
-    public String getTokenUrl() {
-        return tokenUrl;
-    }
-
-    public void setTokenUrl(String tokenUrl) {
-        this.tokenUrl = tokenUrl;
-    }
-
-    public String getUserInfoUrl() {
-        return userInfoUrl;
-    }
-
-    public void setUserInfoUrl(String userInfoUrl) {
-        this.userInfoUrl = userInfoUrl;
-    }
-}
+public record KakaoAuthConfig(
+        String restApiKey,
+        String redirectUri,
+        String tokenUrl,
+        String userInfoUrl
+) { }
