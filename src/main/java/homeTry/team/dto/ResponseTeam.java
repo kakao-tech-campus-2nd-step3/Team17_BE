744f42a0ebae81e69815b6451ebac785ee6f3a5f
Woojae Jeong <woo6388@naver.com>
2024-10-10 17:30:03 +0900

    refactor: 팀 password가 없을 때 null예외를 방지하기 위해 리팩토링

--- src/main/java/homeTry/team/dto/ResponseTeam.java
+++ src/main/java/homeTry/team/dto/ResponseTeam.java
@@ -2,29 +2,35 @@ package homeTry.team.dto;
 
 import homeTry.tag.dto.TagDTO;
 import homeTry.team.model.entity.Team;
+import homeTry.team.model.vo.Password;
 
 import java.util.List;
 
 public record ResponseTeam(
-    Long id,
-    String teamName,
-    String leaderNickname,
-    String teamDescription,
-    long maxParticipants,
-    long currentParticipants,
-    String password,
-    List<TagDTO> tagList
+        Long id,
+        String teamName,
+        String leaderNickname,
+        String teamDescription,
+        long maxParticipants,
+        long currentParticipants,
+        String password,
+        List<TagDTO> tagList
 ) {
 
+
     public static ResponseTeam of(Team team, List<TagDTO> tagList) {
+        String password = team.getPassword()
+                .map(Password::getValue)
+                .orElse(null);
+
         return new ResponseTeam(
-            team.getId(),
-            team.getTeamName().value(),
-            team.getLeader().getNickname(),
-            team.getTeamDescription().value(),
-            team.getMaxParticipants().value(),
-            team.getCurrentParticipants().value(),
-            team.getPassword().value(),
-            tagList);
+                team.getId(),
+                team.getTeamName().value(),
+                team.getLeader().getNickname(),
+                team.getTeamDescription().value(),
+                team.getMaxParticipants().value(),
+                team.getCurrentParticipants().value(),
+                password,
+                tagList);
     }
 }
