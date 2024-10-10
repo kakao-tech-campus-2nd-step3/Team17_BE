a4c998f71bd1011614538357381ae62ac2734451
rdme0 <71927381+rdme0@users.noreply.github.com>
2024-10-10 17:35:31 +0900

    Revert "feat(ChangeNicknameRequest): DTO 유효성 검사 추가"
    This reverts commit 6c5478fd52b1a5d59d3b62dfe19533fcb7e2f4ef.

--- src/main/java/homeTry/member/dto/request/ChangeNicknameRequest.java
+++ src/main/java/homeTry/member/dto/request/ChangeNicknameRequest.java
@@ -1,12 +1,5 @@
 package homeTry.member.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
-public record ChangeNicknameRequest(
-        @NotBlank(message = "닉네임을 입력해주세요.")
-        @Size(max = 32, message = "닉네임의 길이는 32자를 넘을 수 없습니다.")
-        String name
-) {
+public record ChangeNicknameRequest(String name) {
 
 }
