d96488ad98fb9fdc81db07e11b29f79f8aaaf7b1
Woojae Jeong <woo6388@naver.com>
2024-10-13 00:59:31 +0900

    refactor: GlobalExceptionHandler 상속 추가 및 Illegal,NoSuch 예외 처리 구현
    예측할 수 없는 다양한 예외에 대한 처리를 위해 ResponseEntityExceptionHandler 상속

--- src/main/java/homeTry/common/exception/GlobalExceptionHandler.java
+++ src/main/java/homeTry/common/exception/GlobalExceptionHandler.java
@@ -1,14 +1,63 @@
 package homeTry.common.exception;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import homeTry.common.exception.dto.response.ErrorResponse;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.NoSuchElementException;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+        ErrorType errorType = CommonErrorType.METHOD_ARGUMENT_NOT_VALID_EXCEPTION;
+        ErrorResponse errorResponse = new ErrorResponse(
+                errorType.getErrorCode(),
+                errorType.getMessage()
+        );
+        return new ResponseEntity<>(errorResponse, errorType.getHttpStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingPathVariable(
+            MissingPathVariableException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+        ErrorType errorType = CommonErrorType.MISSING_PATH_VARIABLE_EXCEPTION;
+        ErrorResponse errorResponse = new ErrorResponse(
+                errorType.getErrorCode(),
+                errorType.getMessage()
+        );
+        return new ResponseEntity<>(errorResponse, errorType.getHttpStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(
+            MissingServletRequestParameterException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+        ErrorType errorType = CommonErrorType.MISSING_REQUEST_PARAM_EXCEPTION;
+        ErrorResponse errorResponse = new ErrorResponse(
+                errorType.getErrorCode(),
+                errorType.getMessage()
+        );
+        return new ResponseEntity<>(errorResponse, errorType.getHttpStatus());
+    }
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException ex) {
@@ -30,12 +79,18 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorResponse, errorType.getHttpStatus());
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
-        ErrorResponse errorResponse = new ErrorResponse(
-                "INTERNAL_SERVER_ERROR",
-                "An unexpected error occurred"
-        );
-        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException e) {
+        ErrorType errorType = CommonErrorType.NO_SUCH_ARGUMENT_EXCEPTION;
+        ErrorResponse errorResponse = new ErrorResponse(errorType.getErrorCode(), errorType.getMessage());
+        return new ResponseEntity<>(errorResponse, errorType.getHttpStatus());
+    }
+
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        ErrorType errorType = CommonErrorType.ILLEGAL_ARGUMENT_EXCEPTION;
+        ErrorResponse errorResponse = new ErrorResponse(errorType.getErrorCode(), errorType.getMessage());
+        return new ResponseEntity<>(errorResponse, errorType.getHttpStatus());
     }
 }
